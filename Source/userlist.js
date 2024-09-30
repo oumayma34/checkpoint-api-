b0e025d5d19de33253d3225a22fc7cd6902ccb28
oumayma zoughlami <oumayma-90@outlook.fr>
2024-09-30 19:51:20 +0100

    first commit

---
+++ Source/userlist.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  // Fetch des données via l'API dans le hook useEffect
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUsers(response.data);
+      })
+      .catch(error => {
+        console.error("Il y a eu une erreur lors du fetch des utilisateurs!", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Liste des utilisateurs</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
